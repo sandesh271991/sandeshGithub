50f291eafeca0b633286f14f996f9c55f0c73015
sandesh271991 <sardar.sandesh@gmail.com>
2015-09-02 16:11:48 +0530

    firstCommitByRoshan
    first Commit From IMAc

--- GithubSetup/GithubSetup/ViewController.m
+++ GithubSetup/GithubSetup/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"this is first commit from Roshan");
+    
 }
 
 - (void)didReceiveMemoryWarning {
