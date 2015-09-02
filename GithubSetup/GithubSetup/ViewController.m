9202ccea92f7aabda9830db17ca79d9bd7e27b0f
sandesh271991 <sardar.sandesh@gmail.com>
2015-09-02 16:47:52 +0530

    3rd commit

--- GithubSetup/GithubSetup/ViewController.m
+++ GithubSetup/GithubSetup/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"this is first commit from Roshan");
+    NSLog(@"this is first commit from Roshan Dharpure");
     
 }
 
